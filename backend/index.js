9f2d10250b65d56af8ea27b9ce568973be3f3738
KhawajaTanzeelUrRehman <77752933+KhawajaTanzeelUrRehman@users.noreply.github.com>
2022-08-22 13:00:46 +0500

    Video 44

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,15 @@
 const connectToMongo = require('./db');
-const express = require('express');
-connectToMongo();
-const app = express();
-const port = 3000;
+const express = require('express')
 
-app.get('/', (req, res) => {
-    res.send('Hello TANZEEL!')
-})
+connectToMongo();
+const app = express()
+const port = 3000
+
+// Available Routes
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
